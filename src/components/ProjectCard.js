a7c74cab74e6dbe811963b789c04460646977fd3
Gary Peng <49541146+gary-peng@users.noreply.github.com>
2022-04-25 05:10:53 -0400

    Card image fix

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,10 +5,8 @@ import BadgeBtn from './BadgeBtn'
 
 export default function ProjectCard(props) {
     return (
-        <Flex flexDirection="column" width={{ base: "80vw", md: "20vw" }} height={{ base: "60vh", md: "50vh" }} border="1px solid white" borderRadius="7px" m="10px" p="15px" overflowY="auto">
-            <Flex height="30%" justifyContent="center" mb="20px"> 
-                <Image height="100%" objectFit="contain" border="1px solid white" borderRadius="7px" src={props.project.img} />
-            </Flex>
+        <Flex flexDirection="column" width={{ base: "80vw", md: "20vw" }} border="1px solid white" borderRadius="7px" m="10px" p="15px" overflowY="auto">
+            <Image objectFit="contain" borderRadius="7px" mb="20px" src={props.project.img} />
             <Heading fontWeight="normal" textDecoration="underline" size="md" mb="10px">{props.project.name}</Heading>
             <Text fontSize='xs' mb="10px">{props.project.lang}, {props.project.tech}</Text>
             <Text mb="20px">{props.project.desc}</Text>
